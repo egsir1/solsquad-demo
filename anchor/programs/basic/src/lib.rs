663b063c735127d8b5ad33118040049a5a26b91e
Greg Thompson <thompsgj@yahoo.com>
2025-04-05 01:49:17 +0900

    refactor: simplify program to store metadata from ipfn

--- anchor/programs/basic/src/lib.rs
+++ anchor/programs/basic/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 
-pub mod user; 
-use user::*; 
+pub mod user;
+use user::*;
 
 declare_id!("6z68wfurCMYkZG51s1Et9BJEd9nJGUusjHXNt4dGbNNF");
 
@@ -9,30 +9,7 @@ declare_id!("6z68wfurCMYkZG51s1Et9BJEd9nJGUusjHXNt4dGbNNF");
 pub mod survey_app_program {
     use super::*;
 
-    pub fn register_user(
-        ctx: Context<RegisterUser>,
-        username: String,
-        email: String,
-        avatar_ipfs_hash: String,
-    ) -> Result<()> {
-        user::register_user(ctx, username, email, avatar_ipfs_hash)
-    }
-
-    pub fn update_user_account_settings(
-        ctx: Context<UpdateUserAccountSettings>,
-        username: String,
-        email: String,
-        avatar_ipfs_hash: String,
-    ) -> Result<()> {
-        user::update_user_account_settings(ctx, username, email, avatar_ipfs_hash)
-    }
-
-    pub fn update_user_survey_stats(
-        ctx: Context<UpdateUserSurveyStats>,
-        earned_money: u64,
-        surveys_attended: u32,
-        surveys_created: u32,
-    ) -> Result<()> {
-        user::update_user_survey_stats(ctx, earned_money, surveys_attended, surveys_created)
+    pub fn register_user(ctx: Context<RegisterUser>, ipfn_cid: String) -> Result<()> {
+        user::register_user(ctx, ipfn_cid)
     }
 }
--- anchor/programs/basic/src/user.rs
+++ anchor/programs/basic/src/user.rs
@@ -1,6 +1,5 @@
 use anchor_lang::prelude::*;
 
-
 #[derive(Accounts)]
 pub struct RegisterUser<'info> {
     #[account(
@@ -9,107 +8,25 @@ pub struct RegisterUser<'info> {
         space = 8 + User::MAX_SIZE
     )]
     pub user: Account<'info, User>,
-    
+
     #[account(mut)]
     pub signer: Signer<'info>,
 
     pub system_program: Program<'info, System>,
 }
 
-#[derive(Accounts)]
-pub struct UpdateUserAccountSettings<'info> {
-    #[account(mut, has_one = wallet)]
-    pub user: Account<'info, User>,
-    pub wallet: Signer<'info>,
-}
-
-#[derive(Accounts)]
-pub struct UpdateUserSurveyStats<'info> {
-    #[account(mut, has_one = wallet)]
-    pub user: Account<'info, User>,
-    pub wallet: Signer<'info>,
-}
-
-
 #[account]
 pub struct User {
-    pub username: String,
-    pub email: String,
-    pub wallet: Pubkey,
-    pub avatar_ipfs_hash: String,
-    pub earned_money: u64,
-    pub surveys_attended: u32,
-    pub surveys_created: u32, 
-    pub created_at: i64,
-    pub updated_at: i64
+    pub ipfn_cid: String,
 }
 
-/// Defines the maximum size of each field in the User struct
 impl User {
-    pub const MAX_SIZE: usize = 
-        4 + 64 + // username
-        4 + 64 + // email
-        32 +     // wallet
-        4 + 64 + // avatar_ipfs_hash
-        8 +      // earned_money
-        4 +      // surveys_attended
-        4 +      // surveys_created
-        8 +      // created_at
-        8;       // updated_at
+    pub const MAX_SIZE: usize = 4 + 100;
 }
 
-pub fn register_user(
-    ctx: Context<RegisterUser>,
-    username: String,
-    email: String,
-    avatar_ipfs_hash: String,
-) -> Result<()> {
-    let clock = Clock::get()?;
+pub fn register_user(ctx: Context<RegisterUser>, ipfn_cid: String) -> Result<()> {
     let user = &mut ctx.accounts.user;
-
-    user.username = username;
-    user.email = email;
-    user.wallet = *ctx.accounts.signer.key;
-    user.avatar_ipfs_hash = avatar_ipfs_hash;
-    user.earned_money = 0;
-    user.surveys_attended = 0;
-    user.surveys_created = 0;
-    user.created_at = clock.unix_timestamp;
-    user.updated_at = clock.unix_timestamp;
-
+    user.ipfn_cid = ipfn_cid;
     Ok(())
 }
 
-pub fn update_user_account_settings(
-    ctx: Context<UpdateUserAccountSettings>,
-    username: String,
-    email: String,
-    avatar_ipfs_hash: String,
-) -> Result<()> {
-    let clock = Clock::get()?;
-    let user = &mut ctx.accounts.user;
-
-    user.username = username;
-    user.email = email;
-    user.avatar_ipfs_hash = avatar_ipfs_hash;
-    user.updated_at = clock.unix_timestamp;
-
-    Ok(())
-}
-
-pub fn update_user_survey_stats(
-    ctx: Context<UpdateUserSurveyStats>,
-    earned_money: u64,
-    surveys_attended: u32,
-    surveys_created: u32,
-) -> Result<()> {
-    let clock = Clock::get()?;
-    let user = &mut ctx.accounts.user;
-
-    user.earned_money = earned_money;
-    user.surveys_attended = surveys_attended;
-    user.surveys_created = surveys_created;
-    user.updated_at = clock.unix_timestamp;
-
-    Ok(())
-}
