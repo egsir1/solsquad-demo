9208d4d8ed601d8b7d84186ad526fe4cfff25073
Greg Thompson <thompsgj@yahoo.com>
2025-04-05 02:14:18 +0900

    fix: remove unused function and add authority lines

--- anchor/programs/basic/src/survey.rs
+++ anchor/programs/basic/src/survey.rs
@@ -8,28 +8,26 @@ pub struct RegisterSurvey<'info> {
         space = 8 + Survey::MAX_SIZE
     )]
     pub survey: Account<'info, Survey>,
-    pub signer: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
 
-#[derive(Accounts)]
-pub struct UpdateSurvey<'info> {
-    #[account(mut, has_one = creator)]
-    pub survey: Account<'info, Survey>,
-    pub creator: Signer<'info>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
 pub struct Survey {
+    pub authority: Pubkey,
     pub ipfn_cid: String,
 }
 
 impl Survey {
-    pub const MAX_SIZE: usize = 4 + 128;
+    pub const MAX_SIZE: usize = 32 + 4 + 128;
 }
 
 pub fn register_survey(ctx: Context<RegisterSurvey>, ipfn_cid: String) -> Result<()> {
-    ctx.accounts.survey.ipfn_cid = ipfn_cid;
+    let survey = &mut ctx.accounts.survey;
+    survey.ipfn_cid = ipfn_cid;
+    survey.authority = ctx.accounts.signer.key();
     Ok(())
 }
-
