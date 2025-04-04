185318f4b1b977e1f1fd7bd55fee6e4d5ef81a07
Greg Thompson <thompsgj@yahoo.com>
2025-04-04 22:18:24 +0900

    feat: add user management smart contract

--- anchor/programs/basic/src/lib.rs
+++ anchor/programs/basic/src/lib.rs
@@ -1,16 +1,39 @@
 use anchor_lang::prelude::*;
 
+pub mod user; 
+use user::*; 
+
 declare_id!("6z68wfurCMYkZG51s1Et9BJEd9nJGUusjHXNt4dGbNNF");
 
 #[program]
-pub mod basic {
+pub mod user_program {
     use super::*;
 
-    pub fn greet(_ctx: Context<Initialize>) -> Result<()> {
-        msg!("GM!");
-        Ok(())
+    pub fn register_user(
+        ctx: Context<RegisterUser>,
+        username: String,
+        email: String,
+        avatar_ipfs_hash: String,
+    ) -> Result<()> {
+        user::register_user(ctx, username, email, avatar_ipfs_hash)
+    }
+
+    pub fn update_account_settings(
+        ctx: Context<UpdateAccountSettings>,
+        username: String,
+        email: String,
+        avatar_ipfs_hash: String,
+    ) -> Result<()> {
+        user::update_account_settings(ctx, username, email, avatar_ipfs_hash)
+    }
+
+    pub fn update_survey_stats(
+        ctx: Context<UpdateSurveyStats>,
+        earned_money: u64,
+        surveys_attended: u32,
+        surveys_created: u32,
+    ) -> Result<()> {
+        user::update_survey_stats(ctx, earned_money, surveys_attended, surveys_created)
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
